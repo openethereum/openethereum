6021e2b16bc7d69e0c306c8c8d5edd575171160a
adria0.eth <5526331+adria0@users.noreply.github.com>
2020-08-06 12:19:44 +0200

    Update ethcore/evm/src/interpreter/mod.rs
    Co-authored-by: Artem Vorotnikov <artem@vorotnikov.me>

--- ethcore/evm/src/interpreter/mod.rs
+++ ethcore/evm/src/interpreter/mod.rs
@@ -62,7 +62,7 @@ const TWO_POW_96: U256 = U256([0, 0x100000000, 0, 0]); //0x1 00000000 00000000 0
 const TWO_POW_224: U256 = U256([0, 0, 0, 0x100000000]); //0x1 00000000 00000000 00000000 00000000 00000000 00000000 00000000
 const TWO_POW_248: U256 = U256([0, 0, 0, 0x100000000000000]); //0x1 00000000 00000000 00000000 00000000 00000000 00000000 00000000 000000
 
-/// Maximal subroutine stack size as specified in
+/// Maximum subroutine stack size as specified in
 /// https://eips.ethereum.org/EIPS/eip-2315.
 pub const MAX_SUB_STACK_SIZE : usize = 1023;
 

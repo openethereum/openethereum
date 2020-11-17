26ab00b6c7016b3492a09333b1b2325fc4e22c85
Giacomo <giacomo.licari@gmail.com>
2020-11-17 11:16:24 +0100

    Fix deprecated set-env declaration (#106)
    * Fix deprecated set-env declaration
    
    * Fix add-path and set-env in install-sscache.ps1

--- scripts/actions/install-sccache.ps1
+++ scripts/actions/install-sccache.ps1
@@ -15,5 +15,5 @@ curl -LO $url
 tar -xzvf "$basename.tar.gz"
 ls $basename/
 . $basename/sccache --start-server
-echo "::add-path::$(pwd)/$basename"
-echo "::set-env name=RUSTC_WRAPPER::sccache"
+echo "$(pwd)/$basename" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+echo "RUSTC_WRAPPER=sccache" >> $GITHUB_ENV

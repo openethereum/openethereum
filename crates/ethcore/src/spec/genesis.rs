17057eeedcdbf8bf347941cb573dc6c507cab9fb
Dusan Stanivukovic <dusan.stanivukovic@gmail.com>
2021-06-16 20:48:23 +0200

    bump ethereum tests 9.0.2, revert london mainnet block (#435)
    * bump ethereum tests 9.0.2
    * london mainnet block removed

--- crates/ethcore/src/spec/genesis.rs
+++ crates/ethcore/src/spec/genesis.rs
@@ -65,7 +65,7 @@ impl From<ethjson::spec::Genesis> for Genesis {
             state_root: g.state_root.map(Into::into),
             gas_used: g.gas_used.map_or_else(U256::zero, Into::into),
             extra_data: g.extra_data.map_or_else(Vec::new, Into::into),
-            base_fee: g.base_fee.map(Into::into),
+            base_fee: g.base_fee_per_gas.map(Into::into),
         }
     }
 }

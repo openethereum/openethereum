bedc5b32e06cf9a2fb74f9b596c67d6fb4c3404b
Rim Rakhimov <rimrakhimov@gmail.com>
2022-02-16 00:11:15 +0300

    Updates 'engine-api': add PayloadId and ForkchoiceResponse structures

---
+++ crates/engine-api/src/v1/types/forkchoice_response.rs
@@ -0,0 +1,50 @@
+use crate::v1::types::{PayloadId, PayloadStatus, Status};
+use serde::Serialize;
+
+/// Contains response for `engine_forkchoiceUpdatedV1` call.
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ForkchoiceResponse {
+    /// Status of the forkchoice update call.
+    pub payload_status: PayloadStatus,
+    /// Identifier of the payload build process.
+    pub payload_id: Option<PayloadId>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_forkchoice_response_without_payload_id_serialize() {
+        let s = r#"{"payloadStatus":{"status":"VALID","latestValidHash":null,"validationError":null},"payloadId":null}"#;
+        let payload_status = ForkchoiceResponse {
+            payload_status: PayloadStatus {
+                status: Status::Valid,
+                latest_valid_hash: None,
+                validation_error: None,
+            },
+            payload_id: None,
+        };
+
+        let serialized = serde_json::to_string(&payload_status).unwrap();
+        assert_eq!(serialized, s);
+    }
+
+    #[test]
+    fn test_forkchoice_response_with_payload_id_serialize() {
+        let s = r#"{"payloadStatus":{"status":"VALID","latestValidHash":null,"validationError":null},"payloadId":"0xa247243752eb10b4"}"#;
+        let payload_status = ForkchoiceResponse {
+            payload_status: PayloadStatus {
+                status: Status::Valid,
+                latest_valid_hash: None,
+                validation_error: None,
+            },
+            payload_id: Some(PayloadId::from_str("0xa247243752eb10b4").unwrap()),
+        };
+
+        let serialized = serde_json::to_string(&payload_status).unwrap();
+        assert_eq!(serialized, s);
+    }
+}
--- crates/engine-api/src/v1/types/mod.rs
+++ crates/engine-api/src/v1/types/mod.rs
@@ -3,6 +3,8 @@ mod forkchoice_state;
 mod payload_attributes;
 mod payload_status;
 mod transition_configuration;
+mod payload_id;
+mod forkchoice_response;
 
 pub use execution_payload::ExecutionPayload;
 pub use forkchoice_state::ForkchoiceState;
@@ -11,3 +13,4 @@ pub use payload_id::PayloadId;
 pub use payload_status::PayloadStatus;
 pub use payload_status::Status;
 pub use transition_configuration::TransitionConfiguration;
+pub use forkchoice_response::ForkchoiceResponse;

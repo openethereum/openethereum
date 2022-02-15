9b92ef1d46c1d99fe1e381edef75189393bb9cc8
Rim Rakhimov <rimrakhimov@gmail.com>
2022-02-16 00:08:46 +0300

    Updates 'engine-api': make types public outside the module

--- crates/engine-api/src/v1/types/mod.rs
+++ crates/engine-api/src/v1/types/mod.rs
@@ -3,3 +3,11 @@ mod forkchoice_state;
 mod payload_attributes;
 mod payload_status;
 mod transition_configuration;
+
+pub use execution_payload::ExecutionPayload;
+pub use forkchoice_state::ForkchoiceState;
+pub use payload_attributes::PayloadAttributes;
+pub use payload_id::PayloadId;
+pub use payload_status::PayloadStatus;
+pub use payload_status::Status;
+pub use transition_configuration::TransitionConfiguration;
